149e238509b187f14a80cbf589aa9f29820a8453
KUANG, Fangjun <csukuangfj@gmail.com>
2016-09-24 15:58:43 +0200

    Draw two triangles with element buffer object.

---
+++ src/2_hello_triangle_with_element_buffer.cpp
@@ -0,0 +1,167 @@
+//
+// Created by fangjun on 24/09/16.
+//
+
+/*
+ *
+ * For the complete source code, refer to http://learnopengl.com/code_viewer.php?code=getting-started/hellotriangle
+ * For the explanation to the code, refer to http://learnopengl.com/#!Getting-started/Hello-Triangle
+ */
+
+/**
+ *
+ * Note: include GLEW before GLFW !!!
+ *
+ * #define GLEW_STATIC  // to link statically
+ */
+
+
+// GLEW
+// #define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include <fstream>
+
+#include "Shader.hpp"
+
+// Window dimensions
+const static GLuint WIDTH = 800, HEIGHT = 600;
+
+// vertex shader source filename
+const static std::string VERTEX_SHADER_FILENAME = "src/shaders/2_hello_triangle_vertex_shader.txt";
+
+// fragment shader source filename
+const static std::string FRAGMENT_SHADER_FILENAME = "src/shaders/2_hello_triangle_fragment_shader.txt";
+
+
+
+
+std::string loadShaderFromFile(const std::string filename);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+
+int hello_triangle_with_element_buffer()
+{
+    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    glfwInit();
+
+    // refer to http://www.glfw.org/docs/latest/window.html#window_hints
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    // initialize GLEW
+
+    // true -> to use modern techniques
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // set viewport
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    // map normalized device coordinates
+    // [-1, 1] --> [0, 800]
+    //             [0, 600]
+    glViewport(0, 0, width, height);
+
+    Shader shader;
+    shader.setVertexShaderFilename(VERTEX_SHADER_FILENAME);
+    shader.setFragShaderFilename(FRAGMENT_SHADER_FILENAME);
+    shader.linkProgram();
+
+
+    // set the coordinate of the rectangle vertices
+    GLfloat vertices[] = {
+            -0.5f, -0.5f, 0.0f, // left bottom, 0
+            -0.5f, 0.5f, 0.0f,  // left top, 1
+            0.5f, 0.5f, 0.0f,   // right top, 2
+            0.5f, -0.5f, 0.0f,  // right bottom, 3
+    };
+
+    GLuint indices[] = {
+            0,1,2,  // first triangle
+            0,3,2,  // second triangle
+    };
+
+
+    GLuint vbo, ebo, vao;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    // set the current vertex array.
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER ,vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+            0, // index, which attribute
+            3, // number of component per vertex attribute. Each coordinate contains 3 component
+            GL_FLOAT, // data type of each component
+            GL_FALSE, // need to be normalized ?
+            0,  // stride, 0 -> compact, OpenGL will compute it. non-zero --> number of byte between two consecutive vertices
+            nullptr);   // offset
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);   // reset
+    glBindVertexArray(0);
+
+    // uncomment out the following state to draw wire frame.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // clear the color buffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw the triangle
+        shader.useProgram();
+        glBindVertexArray(vao);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        glBindVertexArray(0); // reset;
+
+        glfwSwapBuffers(window);
+    }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+
+    glfwTerminate();
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 
 int hello_world();
 int hello_triangle();
+int hello_triangle_with_element_buffer();
 
-#define TEST hello_triangle
+#define TEST hello_triangle_with_element_buffer
 
 int main(void)
 {
