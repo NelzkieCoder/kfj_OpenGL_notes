5b11724d66b342ca9dce52c50853977964e23ad2
KUANG, Fangjun <csukuangfj@gmail.com>
2016-09-25 09:53:58 +0200

    draw two triangles with different color.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@ int hello_triangle();
 int hello_triangle_with_element_buffer();
 int hello_two_triangles();
 int hello_two_triangles_2();
+int hello_two_triangles_2_with_different_color();
 
-#define TEST hello_two_triangles_2
+#define TEST hello_two_triangles_2_with_different_color
 
 int main(void)
 {
