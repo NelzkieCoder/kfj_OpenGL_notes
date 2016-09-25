4745a1fe804fbc2c4ba782a3caba1aacd42a344b
KUANG, Fangjun <csukuangfj@gmail.com>
2016-09-25 16:30:41 +0200

    draw a triangle and change its color via vertex attribute.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ int hello_two_triangles_2();
 int hello_two_triangles_2_with_different_color();
 int hello_circle();
 int hello_triangle_uniform();
+int hello_triangle_color();
 
-#define TEST hello_triangle_uniform
+#define TEST hello_triangle_color
 
 int main(void)
 {
