c01d75c9d22c243e487171cb315969f018073203
KUANG, Fangjun <csukuangfj@gmail.com>
2016-09-25 10:59:56 +0200

    update comment.

--- src/2_hello_circle.cpp
+++ src/2_hello_circle.cpp
@@ -147,7 +147,7 @@ int hello_circle()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw the triangle
+        // draw the circle (draw a line between two neighboring points)
         shader.useProgram();
         glBindVertexArray(vao);
         glDrawArrays(
